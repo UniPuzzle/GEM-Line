114befe31a667348300de1545d9f629cf40289ab
VitaliiChichuga <chichygav@gmail.com>
2023-11-26 12:11:04 +0200

    update css

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-document.addEventListener("click", (e) => {
-  const svg = e.target;
-  const index = [...svg.children];
-  for (let i = 0; i < index.length; i++) {
-    const element = index[i];
+// document.addEventListener("click", (e) => {
+//   const svg = e.target;
+//   const index = [...svg.children];
+//   for (let i = 0; i < index.length; i++) {
+//     const element = index[i];
 
-    element.style.fill = "red";
-    console.log(element);
-  }
-});
+//     element.style.fill = "red";
+//     console.log(element);
+//   }
+// });
